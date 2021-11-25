c4e2edcc887dfde3056f25f252ae422b4cfa47a0
abraham perez reyes <abrahampre27@gmail.com>
2021-11-25 10:25:57 -0600

    back end

---
+++ client/src/components/ActivityForm/index.js
@@ -0,0 +1,83 @@
+import React, { useState } from 'react';
+
+import { useMutation } from '@apollo/react-hooks';
+import { ADD_ACTIVITY } from '../../utils/mutations';
+import { QUERY_ACTIVITIES, QUERY_ME } from '../../utils/queries';
+
+const ActivityForm = () => {
+  const [type, setText] = useState('');
+  const [characterCount, setCharacterCount] = useState(0);
+
+  const [addActivity, { error }] = useMutation(ADD_ACTIVITY, {
+    update(cache, { data: { addActivity } }) {
+      try {
+        // update thought array's cache
+        // could potentially not exist yet, so wrap in a try/catch
+        const { activities } = cache.readQuery({ query: QUERY_ACTIVITIES });
+        cache.writeQuery({
+          query: QUERY_ACTIVITIES,
+          data: { activities: [addActivity, ...activities] }
+        });
+      } catch (e) {
+        console.error(e);
+      }
+
+      // update me object's cache
+      const { me } = cache.readQuery({ query: QUERY_ME });
+      cache.writeQuery({
+        query: QUERY_ME,
+        data: { me: { ...me, activities: [...me.activities, addActivity] } }
+      });
+    }
+  });
+
+  // update state based on form input changes
+  const handleChange = event => {
+    if (event.target.value.length <= 280) {
+      setText(event.target.value);
+      setCharacterCount(event.target.value.length);
+    }
+  };
+
+  // submit form
+  const handleFormSubmit = async event => {
+    event.preventDefault();
+
+    try {
+      await addActivity({
+        variables: { type }
+      });
+
+      // clear form value
+      setText('');
+      setCharacterCount(0);
+    } catch (e) {
+      console.error(e);
+    }
+  };
+
+  return (
+    <div>
+      <p className={`m-0 ${characterCount === 280 || error ? 'text-error' : ''}`}>
+        Character Count: {characterCount}/280
+        {error && <span className="ml-2">Something went wrong...</span>}
+      </p>
+      <form
+        className="flex-row justify-center justify-space-between-md align-stretch"
+        onSubmit={handleFormSubmit}
+      >
+        <textarea
+          placeholder="Here's a new thought..."
+          value={type}
+          className="form-input col-12 col-md-9"
+          onChange={handleChange}
+        ></textarea>
+        <button className="btn col-12 col-md-3" type="submit">
+          Submit
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default ActivityForm;
