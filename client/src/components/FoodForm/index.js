b16288b41aaa9b0b5a1711333151bc2f34cb9afe
megha <msamala21@gmail.com>
2021-11-27 03:40:37 -0600

    create SingleFood and update FoodForm/List

--- client/src/components/FoodForm/index.js
+++ client/src/components/FoodForm/index.js
@@ -2,21 +2,21 @@ import React, { useState } from 'react';
 
 import { useMutation } from '@apollo/react-hooks';
 import { ADD_THOUGHT } from '../../utils/mutations';
-import { QUERY_THOUGHTS, QUERY_ME } from '../../utils/queries';
+import { QUERY_FOODS, QUERY_ME } from '../../utils/queries';
 
-const ThoughtForm = () => {
-  const [thoughtText, setText] = useState('');
+const FoodForm = () => {
+  const [type, setText] = useState('');
   const [characterCount, setCharacterCount] = useState(0);
 
-  const [addThought, { error }] = useMutation(ADD_THOUGHT, {
-    update(cache, { data: { addThought } }) {
+  const [addFood, { error }] = useMutation(ADD_FOOD, {
+    update(cache, { data: { addFood } }) {
       try {
         // update thought array's cache
         // could potentially not exist yet, so wrap in a try/catch
-        const { thoughts } = cache.readQuery({ query: QUERY_THOUGHTS });
+        const { foods } = cache.readQuery({ query: QUERY_FOODS });
         cache.writeQuery({
-          query: QUERY_THOUGHTS,
-          data: { thoughts: [addThought, ...thoughts] }
+          query: QUERY_FOODS,
+          data: { foods: [addFood, ...foods] }
         });
       } catch (e) {
         console.error(e);
@@ -26,7 +26,7 @@ const ThoughtForm = () => {
       const { me } = cache.readQuery({ query: QUERY_ME });
       cache.writeQuery({
         query: QUERY_ME,
-        data: { me: { ...me, thoughts: [...me.thoughts, addThought] } }
+        data: { me: { ...me, foods: [...me.foods, addFood] } }
       });
     }
   });
@@ -44,8 +44,8 @@ const ThoughtForm = () => {
     event.preventDefault();
 
     try {
-      await addThought({
-        variables: { thoughtText }
+      await addFood({
+        variables: { type }
       });
 
       // clear form value
@@ -80,4 +80,4 @@ const ThoughtForm = () => {
   );
 };
 
-export default ThoughtForm;
+export default FoodForm;
--- client/src/components/FoodList/index.js
+++ client/src/components/FoodList/index.js
@@ -1,33 +1,32 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-const ThoughtList = ({ thoughts, title }) => {
-  if (!thoughts.length) {
-    return <h3>No Thoughts Yet</h3>;
+const FoodList = ({ foods, title }) => {
+  if (!foods.length) {
+    return <h3>No Foods Yet</h3>;
   }
 
   return (
     <div>
       <h3>{title}</h3>
-      {thoughts &&
-        thoughts.map(thought => (
-          <div key={thought._id} className="card mb-3">
+      {foods &&
+        foods.map(food => (
+          <div key={food._id} className="card mb-3">
             <p className="card-header">
               <Link
-                to={`/profile/${thought.username}`}
+                to={`/profile/${food.username}`}
                 style={{ fontWeight: 700 }}
                 className="text-light"
               >
-                {thought.username}
+                {food.username}
               </Link>{' '}
-              thought on {thought.createdAt}
+              food on {food.createdAt}
             </p>
             <div className="card-body">
-              <Link to={`/thought/${thought._id}`}>
-                <p>{thought.thoughtText}</p>
+              <Link to={`/food/${food._id}`}>
+                <p>{food.type}</p>
                 <p className="mb-0">
-                  Reactions: {thought.reactionCount} || Click to{' '}
-                  {thought.reactionCount ? 'see' : 'start'} the discussion!
+                  {/* reactions deleted                */}
                 </p>
               </Link>
             </div>
@@ -37,4 +36,4 @@ const ThoughtList = ({ thoughts, title }) => {
   );
 };
 
-export default ThoughtList;
+export default FoodList;
