427a3bb93eb9fec5991dee662be7442aa3539929
wsbousman <85909621+wsbousman@users.noreply.github.com>
2021-11-29 20:57:19 -0600

    Comment model
    Comment model

--- server/models/index.js
+++ server/models/index.js
@@ -1,5 +1,6 @@
 const User = require('./User');
 const Food = require('./Food');
 const Activity = require('./Activity')
+const Comment = require('./Comment')
 
-module.exports = { User, Food, Activity };
+module.exports = { User, Food, Activity, Comment };
